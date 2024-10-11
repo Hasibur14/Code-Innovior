79f7322f9c4373e96c96c7dedc22907af9a28f11
hasibur14 <hasib141415@gmail.com>
2024-10-11 18:24:45 +0600

    [UPDATE] footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
 import { LiaTelegramPlane } from "react-icons/lia";
-import { VscDebugBreakpointData } from "react-icons/vsc";
+import { PiDotOutlineThin } from "react-icons/pi";
 import footerBg from "../../../assets/footer.png";
 import logo1 from "../../../assets/logo.png";
 
@@ -29,11 +29,10 @@ const coursesGroup2 = [
 const CourseList = ({ courses }) => {
     return (
         <div className="mb-4 md:mb-0 ">
-            <h2 className="text-xl md:text-3xl mb-5">Courses</h2>
-            <ul className="text-[18px] space-y-3">
+            <ul className="text-[15px] space-y-3">
                 {courses?.map((course, index) => (
-                    <li className='cursor-pointer flex gap-2 hover:text-green-500 opacity-80 transition duration-300' key={index}>
-                        <VscDebugBreakpointData className='text-white mt-1 ' />{course}</li>
+                    <li className='cursor-pointer flex gap-2 hover:text-green-500 opacity-80 transition-colors duration-500' key={index}>
+                        < PiDotOutlineThin className='text-white mt-1 ' />{course}</li>
                 ))}
             </ul>
         </div>
@@ -44,16 +43,16 @@ const CourseList = ({ courses }) => {
 const SocialLinks = () => (
     <div className="flex space-x-4">
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Facebook">
-            <FaFacebookF className="text-xl hover:text-green-500 transition" />
+            <FaFacebookF className="text-2xl p-1 rounded bg-[#05B689] text-white hover:bg-white hover:text-[#05B689] transition duration-500" />
         </a>
         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="Follow us on LinkedIn">
-            <FaLinkedinIn className="text-xl hover:text-green-500 transition" />
+            <FaLinkedinIn className="text-2xl p-1 rounded bg-[#05B689] text-white hover:bg-white hover:text-[#05B689] transition duration-500" />
         </a>
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Twitter">
-            <FaXTwitter className="text-xl hover:text-green-500 transition" />
+            <FaXTwitter className="text-2xl p-1 rounded bg-[#05B689] text-white hover:bg-white hover:text-[#05B689] transition duration-500" />
         </a>
         <a href="mailto:info@example.com" target="_blank" rel="noopener noreferrer" aria-label="Email us">
-            <LiaTelegramPlane className="text-xl hover:text-green-500 transition" />
+            <LiaTelegramPlane className="text-2xl p-1 rounded bg-[#05B689] text-white hover:bg-white hover:text-[#05B689] transition duration-500" />
         </a>
     </div>
 );
@@ -62,25 +61,25 @@ const Footer = () => {
     return (
         <footer
             className=" text-white py-6 font-mondo "
-            style={{ background: `linear-gradient(0deg, rgba(46, 179, 145, 0.20) 0%, rgba(46, 179, 145, 0.20) 100%), url(${footerBg}) lightgray 50% / cover no-repeat`
-            }}
-        >
+            style={{
+                background: ` url(${footerBg}) lightgray 50% / cover no-repeat`
+            }} >
             <div className="container mx-auto md:flex  px-4 justify-between">
                 <div className="mb-4 md:mb-0 lg:w-1/3 space-y-6">
-                    <div className="flex items-center mb-2">
-                        <img className='' src={logo1} alt="code innovior" />
-
-                    </div>
-                    <p className="text-[18px] mb-2 text-white opacity-85 ">
+                    <img
+                        src={logo1}
+                        alt="code innovior"/>
+                    <p className="text-[16px] mb-2 text-white text-justify opacity-85 ">
                         Codeinnovior (a concern of goinnovior technology) is a technology-based learning platform. This platform was created by a group of IT specialists with the goal of sharing knowledge with you. Please follow us on social media to stay up to date.
                     </p>
                     <SocialLinks />
                 </div>
-
-
-                <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-44 lg:space-x-60">
-                    <CourseList courses={coursesGroup1} />
-                    <CourseList courses={coursesGroup2} />
+                <div>
+                    <h2 className='text-xl md:text-3xl mb-4 ml-1 text-slate-200 font opacity-80'>Courses</h2>
+                    <div className="flex flex-col text-[12px] md:flex-row space-y-4 md:space-y-0 md:space-x-44 lg:space-x-60">
+                        <CourseList courses={coursesGroup1} />
+                        <CourseList courses={coursesGroup2} />
+                    </div>
                 </div>
             </div>
             <div className='container mx-auto my-6 opacity-85'>
@@ -89,10 +88,10 @@ const Footer = () => {
             <div className="text-center text-sm mt-4">
                 © 2024 Design & Developed By
                 <a
-                    className='hover:text-green-500 font-semibold'
+                    className='hover:text-green-500 transition-colors duration-500 font-semibold'
                     href="https://www.goinnovior.com/"
                     target="_blank"
-                    rel="noopener noreferrer">  CodeInnovior Limited .</a>
+                    rel="noopener noreferrer">  CodeInnovior .</a>
             </div>
         </footer>
     );
