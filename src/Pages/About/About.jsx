077e83ae73eecc28e403d0a5c47a0c0bb4ca87f9
hasibur14 <hasib141415@gmail.com>
2024-10-14 23:33:17 +0600

    [ADD] keen-slider

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,6 @@
 import Journey from "../../components/About/Journey/Journey";
 import OurMission from "../../components/About/OurMission/OurMission";
+import Slider from "../../components/keen-slide/Slider";
 
 
 const About = () => {
@@ -7,6 +8,7 @@ const About = () => {
         <div className="mx-4 md:mx-0">
             <Journey />
             <OurMission />
+            <Slider/>
         </div>
     );
 };
