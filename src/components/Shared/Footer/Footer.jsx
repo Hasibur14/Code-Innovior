09b4f737828bb2f1f68df33a5dd49cbd07d08800
Hasibur14 <hasib141415@gmail.com>
2024-12-03 09:47:15 +0600

    [UPDATE] footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
 import { LiaTelegramPlane } from "react-icons/lia";
@@ -28,17 +29,18 @@ const coursesGroup2 = [
 
 const CourseList = ({ courses }) => {
     return (
-        <div className="mb-4 md:mb-0 ">
+        <div className="mb-4 md:mb-0">
             <ul className="text-[15px] space-y-3">
                 {courses?.map((course, index) => (
-                    <li className='cursor-pointer flex gap-2 hover:text-green-500 opacity-80 transition-colors duration-500' key={index}>
-                        < PiDotOutlineThin className='text-white mt-1 ' />{course}</li>
+                    <li className='cursor-pointer  flex gap-2 hover:text-green-500 opacity-80 hover:underline transition-colors duration-500' key={index}>
+                        <PiDotOutlineThin className='text-white mt-1' />
+                        {course}
+                    </li>
                 ))}
             </ul>
         </div>
     );
 };
-
 
 const SocialLinks = () => (
     <div className="flex space-x-4">
@@ -58,18 +60,22 @@ const SocialLinks = () => (
 );
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear(); // Get the current year dynamically
+
     return (
         <footer
-            className=" text-white py-6 font-mondo "
+            className="text-white py-6 font-mondo"
             style={{
-                background: ` url(${footerBg}) lightgray 50% / cover no-repeat`
-            }} >
-            <div className="container mx-auto md:flex  px-4 justify-between">
+                background: `url(${footerBg}) lightgray 50% / cover no-repeat`
+            }}
+        >
+            <div className="container mx-auto md:flex px-4 justify-between">
                 <div className="mb-4 md:mb-0 lg:w-1/3 space-y-6">
                     <img
                         src={logo1}
-                        alt="code innovior"/>
-                    <p className="text-[16px] mb-2 text-white text-justify opacity-85 ">
+                        alt="code innovior"
+                    />
+                    <p className="text-[16px] mb-2 text-white text-justify opacity-85">
                         Codeinnovior (a concern of goinnovior technology) is a technology-based learning platform. This platform was created by a group of IT specialists with the goal of sharing knowledge with you. Please follow us on social media to stay up to date.
                     </p>
                     <SocialLinks />
@@ -86,12 +92,15 @@ const Footer = () => {
                 <hr />
             </div>
             <div className="text-center text-sm mt-4">
-                © 2024 Design & Developed By
+                © {currentYear} Design & Developed By
                 <a
                     className='hover:text-green-500 transition-colors duration-500 font-semibold'
                     href="https://www.goinnovior.com/"
                     target="_blank"
-                    rel="noopener noreferrer">  CodeInnovior .</a>
+                    rel="noopener noreferrer"
+                >
+                    {" "}CodeInnovior
+                </a>.
             </div>
         </footer>
     );
