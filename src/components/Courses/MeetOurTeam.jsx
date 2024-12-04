1dc261b03b4c7f0a32cf5984fb4683739de914e1
Hasibur14 <hasib141415@gmail.com>
2024-12-04 16:23:53 +0600

    update

--- src/components/Courses/MeetOurTeam.jsx
+++ src/components/Courses/MeetOurTeam.jsx
@@ -16,7 +16,7 @@ const MeetOurTeam = () => {
     const tabs = [
         {
             title: "Web Development",
-            description: "Robust Solutions",
+            description: "Build Your Career",
             image: "https://i.postimg.cc/wBZfMvJd/code.png"
         },
         {
@@ -76,6 +76,7 @@ const MeetOurTeam = () => {
         "UI/UX Design": [
             {
                 name: "Tarif",
+                nickName: "Tarif",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "UI Designer",
                 fb: "https://facebook.com",
@@ -84,6 +85,7 @@ const MeetOurTeam = () => {
             },
             {
                 name: "Supto",
+                nickName: "Supto",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "UX Designer",
                 fb: "https://facebook.com",
@@ -93,7 +95,8 @@ const MeetOurTeam = () => {
         ],
         "Graphics Design": [
             {
-                name: "John",
+                name: "Redhi",
+                nickName: "Redhi",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Graphic Artist",
                 fb: "https://facebook.com",
@@ -101,7 +104,8 @@ const MeetOurTeam = () => {
                 github: "https://github.com"
             },
             {
-                name: "Doe",
+                name: "Ashik",
+                nickName: "Ashik",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Graphic Designer",
                 fb: "https://facebook.com",
@@ -112,6 +116,7 @@ const MeetOurTeam = () => {
         "Cyber Security": [
             {
                 name: "Tasnim Tanim",
+                nickName: "Tanim",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Cyber Security Analyst",
                 fb: "https://facebook.com",
@@ -119,7 +124,8 @@ const MeetOurTeam = () => {
                 github: "https://github.com"
             },
             {
-                name: "Dave",
+                name: "Minul",
+                nickName: "Minul",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Security Engineer",
                 fb: "https://facebook.com",
@@ -130,7 +136,8 @@ const MeetOurTeam = () => {
         ],
         "SQA": [
             {
-                name: "Tasnim Tanim",
+                name: "Mahabub Jamil",
+                nickName: "Mahbub",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Cyber Security Analyst",
                 fb: "https://facebook.com",
@@ -138,7 +145,8 @@ const MeetOurTeam = () => {
                 github: "https://github.com"
             },
             {
-                name: "Dave",
+                name: "Supto",
+                nickName: "Supto",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Security Engineer",
                 fb: "https://facebook.com",
@@ -149,7 +157,8 @@ const MeetOurTeam = () => {
         ],
         "HR & General": [
             {
-                name: "Tasnim Tanim",
+                name: "Jhamur",
+                nickName: "Jhamur",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Cyber Security Analyst",
                 fb: "https://facebook.com",
@@ -158,6 +167,7 @@ const MeetOurTeam = () => {
             },
             {
                 name: "Dave",
+                nickName: "Dave",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Security Engineer",
                 fb: "https://facebook.com",
@@ -168,7 +178,8 @@ const MeetOurTeam = () => {
         ],
         "Business Development": [
             {
-                name: "Tasnim Tanim",
+                name: "Mahabub",
+                nickName: "Mahbub",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Cyber Security Analyst",
                 fb: "https://facebook.com",
@@ -176,7 +187,8 @@ const MeetOurTeam = () => {
                 github: "https://github.com"
             },
             {
-                name: "Dave",
+                name: "Taj Khan",
+                nickName: "Taj",
                 img: "https://i.postimg.cc/L8X7sQ1f/removal-ai-3d5405ee-684d-4749-ac8b-1ad33ce41f0f-photo-gallery-img-02.png",
                 designation: "Security Engineer",
                 fb: "https://facebook.com",
@@ -189,7 +201,7 @@ const MeetOurTeam = () => {
 
 
     return (
-        <div className="bg-gradient-to-t from-[#160f25] to-[#060606] font-mondo">
+        <div className="bg-gradient-to-t from-[#181129] to-[#0a050d] font-mondo py-5 md:py-28">
 
             <div className="container mx-auto mt-20">
                 <div className="w-full flex items-center justify-center my-20">
@@ -212,14 +224,17 @@ const MeetOurTeam = () => {
                                 <SwiperSlide key={tab.title}>
                                     <button
                                         onClick={() => setActiveTab(tab.title)}
-                                        className={`flex w-full gap-3 px-4 py-2 rounded-full h-20 hover:border-[#5FEAC7] ${activeTab === tab.title
+                                        className={`flex w-full gap-8 px-4 py-2 rounded-full h-20 hover:border-[#5FEAC7] transition-all duration-1000 ${activeTab === tab.title
                                             ? "bg-opacity-50 rounded-full border-2 border-[#5FEAC7] bg-[#5FEAC7] text-[#3affa3]"
                                             : "bg-gray-700 text-gray-300 bg-opacity-30 border-2"
                                             }`}
                                     >
                                         <div className="text-2xl">
                                             <img
-                                                className="bg-gray-600 bg-opacity-80 rounded-full w-14 h-14 p-4"
+                                                className={`ml-3 ${activeTab === tab.title
+                                                    ? "bg-[#3affa3] bg-opacity-20 rounded-full w-14 h-14 p-4"
+                                                    : "bg-gray-600 bg-opacity-80 rounded-full w-14 h-14 p-4"
+                                                    }`}
                                                 src={tab.image}
                                                 alt={tab.title}
                                             />
@@ -260,20 +275,22 @@ const MeetOurTeam = () => {
                                     />
 
                                     <div
-                                        className="absolute inset-0 bg-gradient-to-t from-[#3c9786] to-[#1e493e] opacity-0 group-hover:opacity-90 transition duration-500 bg-[length:100%_20%] bg-bottom"
+                                        className="absolute inset-0 bg-gradient-to-t from-[#3c9786] to-[#1e493e] opacity-0 group-hover:opacity-90 transition duration-500"
                                         style={{
-                                            background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 229, 174, 0.7) 100%)',
+                                            background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 229, 174, 0.63) 100%)',
+
                                         }}
                                     ></div>
 
 
 
+
                                     {/* Content */}
                                     <div className="absolute inset-0 flex bottom-10 flex-col items-center justify-end opacity-0 group-hover:opacity-100 transition duration-500">
-                                        <h4 className="text-xl font-bold transform translate-y-10 group-hover:translate-y-0 transition duration-500">
+                                        <h4 className="text-2xl font-bold transform translate-y-10 group-hover:translate-y-0 transition duration-500">
                                             {item.name}
                                         </h4>
-                                        <p className="text-sm text-gray-200 mb-3 transform translate-y-10 group-hover:translate-y-0 transition duration-500">
+                                        <p className="text-lg text-gray-200 mb-3 transform translate-y-10 group-hover:translate-y-0 transition duration-500">
                                             {item.designation}
                                         </p>
 
